f2c7b63307ce455ccc44093943d8ecc00f73ee5e
Ben <benjaminjgbarnes@gmail.com>
2019-08-21 18:04:38 +1000

    Initial commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public new string name;
+    public string description;
+
+    public Sprite cardArt;
+    public Sprite charClass;
+
+
+    public int damage;
+    public int cost;
+}
+
+public enum CardType
+{
+    ATTACK = 0,
+    SKILL = 1,
+    POWER = 2
+}
---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public TextMeshProUGUI cardName;
+    public TextMeshProUGUI descriptionText;
+
+    public Image artworkImage;
+    public Image cardClassImage;
+    public Image background;
+    public CardType cardType;
+
+    public Text costText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardName.text = card.name;
+        descriptionText.text = card.description;
+
+        artworkImage.sprite = card.cardArt;
+        cardClassImage.sprite = card.charClass;
+
+        switch (cardType)
+        {
+            case CardType.ATTACK:
+                background.color = Color.red;
+                break;
+            case CardType.SKILL:
+                background.color = Color.green;
+                break;
+            case CardType.POWER:
+                background.color = Color.blue;
+                break;
+            default:
+                background.color = new Color(0.4156f, 0.4f, .4156f);
+                break;
+        }
+
+        costText.text = card.cost.ToString();
+    }
+}
